2ab020356e3ca4f8628d2d577f83683b0e434c4c
DarrenKwonDev <anatomy1545@gmail.com>
2022-06-17 18:05:19 +0900

    feat: simple repl

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/go-interpreter/repl"
+)
+
 func main() {
-	println("Hello, world!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
 }
